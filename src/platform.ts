b448352074635e0e1cb7b6e574b5bb0f4f531d5e
Bram Smulders <source@bram.is>
2021-03-28 11:07:18 +0200

    #2 added types & commented dead code

--- src/platform.ts
+++ src/platform.ts
@@ -13,6 +13,18 @@ import request from 'request-promise';
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { SlideLocalAccessory } from './localAccessory';
 
+type device = {
+  name: string;
+  ip: string;
+  code: string;
+  tolerance: number;
+  pollInterval: number;
+};
+
+type parameters = {
+  pos: number;
+};
+
 export class SlidePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap
@@ -54,36 +66,36 @@ export class SlidePlatform implements DynamicPlatformPlugin {
     this.registerDevices(this.devices);
   }
 
-  discoverRemoteDevices() {
-    this.getAccessToken()
-      .then(() => {
-        this.request(null, 'GET', 'slides/overview', null, true)
-          .then((response: any) => {
-            const slides = response.slides;
-            if (slides) {
-              const devices: any = [];
-              slides.forEach((slideInfo: any) => {
-                devices.push({
-                  name: slideInfo['device_name'],
-                  id: slideInfo['id'],
-                  device_id: slideInfo['device_id'],
-                  ip: null,
-                  code: null,
-                });
-              });
-              this.registerDevices(devices);
-            }
-          })
-          .catch((error) => {
-            this.log.error(error);
-          });
-      })
-      .catch((error) => {
-        this.log.error(error);
-      });
-  }
+  // discoverRemoteDevices() {
+  //   this.getAccessToken()
+  //     .then(() => {
+  //       this.request(null, 'GET', 'slides/overview', null, true)
+  //         .then((response: any) => {
+  //           const slides = response.slides;
+  //           if (slides) {
+  //             const devices: any = [];
+  //             slides.forEach((slideInfo: any) => {
+  //               devices.push({
+  //                 name: slideInfo['device_name'],
+  //                 id: slideInfo['id'],
+  //                 device_id: slideInfo['device_id'],
+  //                 ip: null,
+  //                 code: null,
+  //               });
+  //             });
+  //             this.registerDevices(devices);
+  //           }
+  //         })
+  //         .catch((error) => {
+  //           this.log.error(error);
+  //         });
+  //     })
+  //     .catch((error) => {
+  //       this.log.error(error);
+  //     });
+  // }
 
-  registerDevices(devices: any) {
+  registerDevices(devices: device[]) {
     for (const device of devices) {
       const uuid = this.api.hap.uuid.generate(
         `${device.code || device['device_id']}`,
@@ -118,120 +130,57 @@ export class SlidePlatform implements DynamicPlatformPlugin {
     }
   }
 
-  getAccessToken() {
-    return new Promise((resolve, reject) => {
-      if (this.accessToken) {
-        this.log.debug('Already found access token');
-        return resolve(this.accessToken);
-      } else {
-        this.log.debug('Needs access token to continue, returning login');
-        this.login()
-          .then((accessToken) => {
-            resolve(accessToken);
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      }
-    });
-  }
+  // getAccessToken() {
+  //   return new Promise((resolve, reject) => {
+  //     if (this.accessToken) {
+  //       this.log.debug('Already found access token');
+  //       return resolve(this.accessToken);
+  //     } else {
+  //       this.log.debug('Needs access token to continue, returning login');
+  //       this.login()
+  //         .then((accessToken) => {
+  //           resolve(accessToken);
+  //         })
+  //         .catch((error) => {
+  //           reject(error);
+  //         });
+  //     }
+  //   });
+  // }
 
-  login() {
-    if (this.loginPromise) {
-      this.log.debug('Already logging in, returning existing promise');
-      return this.loginPromise;
-    }
-    const parameters = {
-      email: this.config['email'] || this.config['username'],
-      password: this.config['password'],
-    };
-    const promise = new Promise((resolve, reject) => {
-      this.request(null, 'POST', 'auth/login', parameters)
-        .then((response: any) => {
-          this.loginPromise = null;
-          if (!response.access_token) {
-            return reject(Error('Invalid response'));
-          }
-          this.accessToken = response.access_token;
-          return resolve(response.access_token);
-        })
-        .catch((error) => {
-          this.loginPromise = null;
-          return reject(error);
-        });
-    });
-    this.loginPromise = promise;
-    return promise;
-  }
-
-  request(
-    device,
-    method,
-    endPoint,
-    parameters: any | false = false,
-    useToken = false,
-  ) {
-    this.log.debug('Triggered platform request');
-
-    if (endPoint.length > 0 && endPoint.charAt(0) !== '/') {
-      endPoint = '/' + endPoint;
-    }
-
-    const baseURL =
-      device && device.ip
-        ? `http://${device.ip}`
-        : 'https://api.goslide.io/api';
-
-    const addParameters = method === 'POST' && parameters;
-    const authUsingCode = !useToken && device && device.code;
-
-    const requestInfo = {
-      uri: baseURL + endPoint,
-      method: method,
-      timeout: 6000,
-      headers: {
-        'User-Agent': 'homebridge-slide-link',
-      },
-      json: true,
-      ...(useToken && {
-        auth: {
-          bearer: this.accessToken,
-          sendImmediately: true,
-        },
-      }),
-      ...(authUsingCode && {
-        auth: {
-          username: 'user',
-          password: device.code,
-          sendImmediately: false,
-        },
-      }),
-      ...(addParameters && {
-        body: parameters,
-      }),
-    };
-
-    return new Promise((resolve, reject) => {
-      request(requestInfo, (error, response, responseBody) => {
-        if (error) {
-          this.log.error(error);
-          return reject(error);
-        }
-        if (response.statusCode < 200 || response.statusCode >= 300) {
-          error = Error('Invalid response received: ' + response.statusCode);
-          this.log.error(error);
-          return reject(error);
-        }
-        return resolve(responseBody);
-      });
-    });
-  }
+  // login() {
+  //   if (this.loginPromise) {
+  //     this.log.debug('Already logging in, returning existing promise');
+  //     return this.loginPromise;
+  //   }
+  //   const parameters = {
+  //     email: this.config['email'] || this.config['username'],
+  //     password: this.config['password'],
+  //   };
+  //   const promise = new Promise((resolve, reject) => {
+  //     this.request(null, 'POST', 'auth/login', parameters)
+  //       .then((response: any) => {
+  //         this.loginPromise = null;
+  //         if (!response.access_token) {
+  //           return reject(Error('Invalid response'));
+  //         }
+  //         this.accessToken = response.access_token;
+  //         return resolve(response.access_token);
+  //       })
+  //       .catch((error) => {
+  //         this.loginPromise = null;
+  //         return reject(error);
+  //       });
+  //   });
+  //   this.loginPromise = promise;
+  //   return promise;
+  // }
 
   async asyncRequest(
     device,
     method,
     endPoint,
-    parameters: any | false = false,
+    parameters: parameters | false = false,
     useToken = false,
   ) {
     this.log.debug('Triggered platform async request');
